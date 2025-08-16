e6a3bd508e8aad8a6f5f9d1259ba93bd5d5278e7
ayras <AyrasGraydan000@yandex.ru>
2025-08-17 00:39:25 +0300

    KSL-2-1 create arithmetic mean

---
+++ src/Lesson_2/Lesson2_test1.kt
@@ -0,0 +1,25 @@
+package Lesson_2
+
+fun main() {
+
+    val student1: UByte = 3u
+    val student2: UByte = 4u
+    val student3: UByte = 3u
+    val student4: UByte = 5u
+
+    println((3+4+3+5)/4.toFloat())
+
+//    var class1 = Class(listOf(2,5,4,3,5,4,5,5,4,3,2,2,4,42))
+//    println(class1.arithmeticMean())
+//
+//
+//    var a = class1.arithmeticMean()
+//    println(a)
+
+}
+
+//class Class(var marks: List<Int>){
+//
+//    fun arithmeticMean() = "%.2f".format(marks.sum()/marks.size.toFloat())
+//
+//}
