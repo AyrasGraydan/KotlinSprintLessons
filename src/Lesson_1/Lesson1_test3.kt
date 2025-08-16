3c4be5fa96b96500e197efa995c00723dd627fdd
ayras <AyrasGraydan000@yandex.ru>
2025-08-16 17:59:48 +0300

    KLS-1-3 space flight

---
+++ src/Lesson_1/Lesson1_test3.kt
@@ -0,0 +1,21 @@
+package Lesson_1
+
+const val  year: UShort = 1961u
+
+fun main() {
+
+    var  hour: UByte = 9u
+    var  minute: UByte = 7u
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    println()
+
+    hour = 10u
+    minute = 55u
+
+    println("$hour:$minute")
+
+}
