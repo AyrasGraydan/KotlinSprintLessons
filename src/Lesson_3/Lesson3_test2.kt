b355bf62a7a01d0251ff82e4a1357f62644bfd58
ayras <AyrasGraydan000@yandex.ru>
2025-08-18 20:59:38 +0300

    KS-3-2-create info output

---
+++ src/Lesson_3/Lesson3_test2.kt
@@ -0,0 +1,16 @@
+package Lesson_3
+
+fun main() {
+
+    val name = "Татьяна"
+    val secondName = "Андреева"
+    val surname = "Сергеевна"
+
+    var age = 20
+
+    println("$secondName $name $surname, $age")
+
+    age += 2
+
+    println("$secondName $name $surname, $age")
+}
