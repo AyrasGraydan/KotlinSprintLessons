da5246e32a21605827a945402430746a9e580640
ayras <AyrasGraydan000@yandex.ru>
2025-08-18 19:14:46 +0300

    KS-2-4-fixed magic numbers

--- src/Lesson_2/Lesson2_test4.kt
+++ src/Lesson_2/Lesson2_test4.kt
@@ -1,14 +1,16 @@
 package Lesson_2
 
+const val BAFF_BONUS = 0.2
+
 fun main() {
 
-    var crystal = 7
-    var ironOre = 11
+    val crystal = 7
+    val ironOre = 11
 
-    var crystalBaff = (crystal * 1.2 - crystal).toInt()
-    var ironOreBaff = (ironOre * 1.2 - ironOre).toInt()
+    val crystalBaff = (crystal * BAFF_BONUS).toInt()
+    val ironOreBaff = (ironOre * BAFF_BONUS).toInt()
 
     println("Вы получили +$crystalBaff \"кристалл\" от действия баффа")
-    println("Вы получили +$ironOreBaff \"рудного железа\" от действия баффа")
+    println("Вы получили +$ironOreBaff \"рудное железо\" от действия баффа")
 
 }
