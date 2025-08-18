b364f6847a333dc05ca56ff0371abf9eccab2bd3
ayras <AyrasGraydan000@yandex.ru>
2025-08-18 19:05:07 +0300

    KS-2-1-fixed magic numbers

--- src/Lesson_2/Lesson2_test1.kt
+++ src/Lesson_2/Lesson2_test1.kt
@@ -2,24 +2,30 @@ package Lesson_2
 
 fun main() {
 
-    val student1: UByte = 3u
-    val student2: UByte = 4u
-    val student3: UByte = 3u
-    val student4: UByte = 5u
+    //variant 1
 
-    println((3+4+3+5)/4.toFloat())
+    val student1: Byte = 3
+    val student2: Byte = 4
+    val student3: Byte = 3
+    val student4: Byte = 5
 
-//    var class1 = Class(listOf(2,5,4,3,5,4,5,5,4,3,2,2,4,42))
-//    println(class1.arithmeticMean())
-//
-//
-//    var a = class1.arithmeticMean()
-//    println(a)
+    val numberOfMarks = 4
+
+    println((student1 + student2 + student3 + student4) / numberOfMarks.toFloat())
+
+    //variant 2
+
+    /* var class1 = ArithmeticMean(listOf(2,5,4,3,5,4,5,5,4,3,2,2,4,42))
+     println(class1.arithmeticMean())
+
+
+     var a = class1.arithmeticMean()
+     println(a)*/
 
 }
 
-//class Class(var marks: List<Int>){
-//
-//    fun arithmeticMean() = "%.2f".format(marks.sum()/marks.size.toFloat())
-//
-//}
+/*class ArithmeticMean(var marks: List<Int>){
+
+    fun arithmeticMean() = "%.2f".format(marks.sum()/marks.size.toFloat())
+
+}*/
