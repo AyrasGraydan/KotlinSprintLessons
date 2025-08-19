3cad8cf3d65e236f051c5ea5daec385cafda550b
ayras <AyrasGraydan000@yandex.ru>
2025-08-19 18:32:04 +0300

    KS-4-3-favorable-conditions

---
+++ src/Lesson_4/Lesson4_test3.kt
@@ -0,0 +1,34 @@
+package Lesson_4
+
+
+const val FAVORABLE_WEATHER = "солнечная"
+const val FAVORABLE_AWNING = "открыт"
+const val FAVORABLE_AIR_HUMIDITY = 20
+
+const val UNFAVORABLE_TIME_OF_YEAR = "зима"
+
+
+fun main() {
+
+//    - наличие солнечной погоды
+//    - открытый тент
+//    - влажность воздуха составляет 20%
+//    - текущее время года - не зима
+
+//    - погода сегодня - солнечная
+//    - тент на данный момент - открыт
+//    - влажность воздуха - 20%
+//    - текущее время года - зима
+
+    val weather = "солнечная"
+    val awning = "открыт"
+    val airHumidity = 20
+    val timeOfYear = "зима"
+
+
+    val favorableConditions =
+        weather == FAVORABLE_WEATHER && awning == FAVORABLE_AWNING && airHumidity == FAVORABLE_AIR_HUMIDITY && timeOfYear != UNFAVORABLE_TIME_OF_YEAR
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
+
+}
