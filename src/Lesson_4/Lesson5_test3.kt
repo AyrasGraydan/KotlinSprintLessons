97c0fecd1181868901ef19950ca5a5b0ec60a993
ayras <AyrasGraydan000@yandex.ru>
2025-08-20 17:28:25 +0300

    KS-5-3-lottery

---
+++ src/Lesson_4/Lesson5_test3.kt
@@ -0,0 +1,18 @@
+package Lesson_4
+
+import java.io.IO.readln
+
+fun main() {
+
+    val numberOne = 28
+    val numberTwo = 42
+
+    val possibleNumber1 = readln("Введите возможное число: ").toInt()
+    val possibleNumber2 = readln("Введите возможное число: ").toInt()
+
+    if ((possibleNumber1 == numberOne && possibleNumber2 == numberTwo) or (possibleNumber1 == numberTwo && possibleNumber2 == numberOne))
+        println("Поздравляем! Вы выиграли главный приз!")
+    else if (possibleNumber1 == numberOne || possibleNumber2 == numberTwo || possibleNumber1 == numberTwo || possibleNumber2 == numberOne)
+        println("Вы выиграли утешительный приз!")
+    else println("Неудача!")
+}
