a84ea3d62bf06e6c69d58716ab245b24042219c2
ayras <AyrasGraydan000@yandex.ru>
2025-08-20 17:01:20 +0300

    KS-5-1-authorization

---
+++ src/Lesson_4/Lesson5_test1.kt
@@ -0,0 +1,13 @@
+package Lesson_4
+
+import java.io.IO.readln
+
+fun main() {
+
+    val example = "5 + 2 = "
+    val answer = readln(example).toInt()
+
+
+    if (answer == 7) println("Добро пожаловать")
+    else println("Доступ запрещен")
+}
