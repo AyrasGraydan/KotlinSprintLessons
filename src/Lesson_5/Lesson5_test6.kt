7eb7180a39a0d32a8f0ab2a241ba90ad150091fb
ayras <AyrasGraydan000@yandex.ru>
2025-08-21 14:44:52 +0300

    KS-5-6-BMI

---
+++ src/Lesson_5/Lesson5_test6.kt
@@ -0,0 +1,25 @@
+package Lesson_5
+
+import java.io.IO.readln
+import kotlin.math.pow
+
+const val CENTIMETERS_IN_METERS = 100
+
+fun main() {
+
+    val weightKg = readln("Введите ваш вес в килограммах: ").toFloat()
+    val heightCm = readln("Введите ваш рост в сантиметрах: ").toFloat()
+
+    val heightM = (heightCm / CENTIMETERS_IN_METERS).toDouble()
+
+    val bodyMassIndex = weightKg / heightM.pow(2.0).toFloat()
+
+    when {
+        bodyMassIndex < 18.5 -> println("Недостаточная масса тела")
+        bodyMassIndex in 18.5..24.9 -> println("Нормальная масса тела")
+        bodyMassIndex in 25.0..29.9 -> println("Избыточная масса тела")
+        bodyMassIndex >= 30.0 -> println("Ожирение")
+        else -> println("Некорректное значение ИМТ")
+    }
+
+}
