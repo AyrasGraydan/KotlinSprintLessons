9988575e93b4b48e9ab5c5a5a9d081e1ee9d2f26
ayras <AyrasGraydan000@yandex.ru>
2025-08-23 00:56:52 +0300

    KS-3-2-add fun split

--- src/Lesson_3/Lesson3_test5.kt
+++ src/Lesson_3/Lesson3_test5.kt
@@ -4,17 +4,12 @@ fun main() {
 
     val coordinate = "D2-D4;0"
 
-    val whereFrom = coordinate[0].toString() + coordinate[1]
-    val whereTo = coordinate[3].toString() + coordinate[4]
-    val strokeNumber = coordinate[6]
+
+    val whereFrom = coordinate.split("-", ";")[0]
+    val whereTo = coordinate.split("-", ";")[1]
+    val strokeNumber = coordinate.split("-", ";")[2]
 
     println(whereFrom)
     println(whereTo)
     println(strokeNumber)
-
-//    find() — возвращает первое совпадение регулярного выражения в строке в виде объекта MatchResult или null, если совпадение не найдено.
-//    findAll() — возвращает все совпадения в строке, возвращает последовательность объектов MatchResult.
-//    replace() — заменяет все совпадения регулярного выражения в строке указанной строкой замены.
-//    split() — разделяет строку по совпадениям регулярного выражения.
-
 }
