2174cd28ec1b408bf702a24f30b956af7d0a8ff1
ayras <AyrasGraydan000@yandex.ru>
2025-08-30 19:51:15 +0300

    KS-6-5-bot test

---
+++ src/Lesson_6/Lesson6_test5.kt
@@ -0,0 +1,41 @@
+package Lesson_6
+
+import java.io.IO.readln
+import kotlin.random.Random
+
+fun main() {
+    //Добро пожаловать!
+
+    val minNumber = 1
+    val maxNumber = 9
+
+    var number1 = 1
+    var number2 = 1
+    var userNumber = 0
+
+    var numberOfAttempts = 3
+
+
+    println("Отстои свою честь! Докажи, что ты не бот.")
+
+    while (userNumber != number1 + number2 && numberOfAttempts > 0) {
+
+        when (numberOfAttempts) {
+            2 -> println("Целься метче, воин!")
+            1 -> println("У тебя все шансы, сосредоточься...")
+        }
+
+        number1 = Random.nextInt(minNumber, maxNumber + 1)
+        number2 = Random.nextInt(minNumber, maxNumber + 1)
+
+        userNumber = readln("$number1 + $number2 = ").toInt()
+
+        numberOfAttempts--
+    }
+
+    if (userNumber == number1 + number2){
+        println("Я верил, что ты чист!")
+        println("Двери тебе открыты..")
+    } else println("Серое и безжизненное существо...")
+
+}
