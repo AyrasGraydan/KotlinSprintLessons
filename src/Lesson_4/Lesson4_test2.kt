a05bf6d08cabd42dfd828ec676e3cf65d80e8b8c
ayras <AyrasGraydan000@yandex.ru>
2025-08-31 18:24:37 +0300

    KS-4-2-optimized long lines

--- src/Lesson_4/Lesson4_test2.kt
+++ src/Lesson_4/Lesson4_test2.kt
@@ -9,12 +9,17 @@ fun main() {
     var weightInKg = 20
     var volumeInLiters = 80
 
-    println("Груз с весом $weightInKg кг и объемом $volumeInLiters л соответствует категории 'Average': ${MIN_WEIGHT_AVERAGE < weightInKg && weightInKg <= MAX_WEIGHT_AVERAGE && volumeInLiters < MAX_VOLUME_AVERAGE}")
+    println(
+        "Груз с весом $weightInKg кг и объемом $volumeInLiters л соответствует категории 'Average': " +
+                "${MIN_WEIGHT_AVERAGE < weightInKg && weightInKg <= MAX_WEIGHT_AVERAGE && volumeInLiters < MAX_VOLUME_AVERAGE}"
+    )
 
     weightInKg = 50
     volumeInLiters = 100
 
-    println("Груз с весом $weightInKg кг и объемом $volumeInLiters л соответствует категории 'Average': ${MIN_WEIGHT_AVERAGE < weightInKg && weightInKg <= MAX_WEIGHT_AVERAGE && volumeInLiters < MAX_VOLUME_AVERAGE}")
-
+    println(
+        "Груз с весом $weightInKg кг и объемом $volumeInLiters л соответствует категории 'Average': " +
+                "${MIN_WEIGHT_AVERAGE < weightInKg && weightInKg <= MAX_WEIGHT_AVERAGE && volumeInLiters < MAX_VOLUME_AVERAGE}"
+    )
 
 }
