da426f21e711a3dbc374023484a52f032b53fffc
ayras <AyrasGraydan000@yandex.ru>
2025-08-31 18:20:20 +0300

    KS-4-1-change of meaning

--- src/Lesson_4/Lesson4_test1.kt
+++ src/Lesson_4/Lesson4_test1.kt
@@ -5,11 +5,10 @@ const val TOTAL_TABLES = 13
 fun main() {
 
     val bookedTablesToday = 13
-    val bookedTablesTomorrow = 12
+    val bookedTablesTomorrow = 9
 
     val freeTablesToday = bookedTablesToday < TOTAL_TABLES
     val freeTablesTomorrow = bookedTablesTomorrow < TOTAL_TABLES
-
 
     println("Доступность столиков на сегодня: $freeTablesToday" + "\nДоступность столиков на завтра: $freeTablesTomorrow")
 
