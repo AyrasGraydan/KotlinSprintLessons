d7acaecd05f2b1a4f8c3b6d207f4f6b29f58830c
ayras <AyrasGraydan000@yandex.ru>
2025-09-01 11:36:21 +0300

    KS-4-3-names corrected

--- src/Lesson_4/Lesson4_test3.kt
+++ src/Lesson_4/Lesson4_test3.kt
@@ -1,24 +1,21 @@
 package Lesson_4
 
-const val FAVORABLE_WEATHER = true
-const val FAVORABLE_AWNING = true
-const val FAVORABLE_AIR_HUMIDITY = 20
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 
+const val FAVORABLE_AIR_HUMIDITY = 20
 const val UNFAVORABLE_TIME_OF_YEAR = "зима"
 
 fun main() {
 
-    val weather = "солнечная"
-    val awning = "открыт"
+    val isSunny = true
+    val isTentOpen = true
     val airHumidity = 20
     val timeOfYear = "зима"
 
-    val favorableWeather = if (weather == "солнечная") true; else false
-    val favorableAwing = if (awning == "открыт") true; else false
-
     val favorableConditions =
-        favorableWeather == FAVORABLE_WEATHER &&
-                favorableAwing == FAVORABLE_AWNING &&
+        isSunny == IS_SUNNY &&
+                isTentOpen == IS_AWNING_OPEN &&
                 airHumidity == FAVORABLE_AIR_HUMIDITY &&
                 timeOfYear != UNFAVORABLE_TIME_OF_YEAR
 
