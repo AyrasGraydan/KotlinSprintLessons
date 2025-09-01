8326e3defdd49515f5a2d4d354788af790a8a46c
ayras <AyrasGraydan000@yandex.ru>
2025-09-01 15:01:53 +0300

    KS-7-5-password randomizer

---
+++ src/Lesson_7/Lesson7_test5.kt
@@ -0,0 +1,42 @@
+package Lesson_7
+
+const val LENGTH_ALPHABET = 26
+const val DECIMAL_NUMBER_SYSTEM = 10
+
+fun main() {
+
+    val minLength = 6
+    var password = ""
+
+    print("Введите длинну пароля: ")
+    val passwordLength = readln().toInt().coerceAtLeast(minLength)
+
+    while (password.length != passwordLength) {
+
+        val numberOfCharacterTypes = 3
+        val probability = (Math.random() * numberOfCharacterTypes).toInt()
+
+        if (probability != 0) {
+
+            for (i in 'a'..'z') {
+                val randomNumber = (Math.random() * LENGTH_ALPHABET).toInt()
+
+                if (randomNumber == 0) {
+                    val randomNumber = (Math.random() * 2).toInt()
+
+                    if (randomNumber == 0) password += i.uppercase()
+                    else password += i
+                }
+            }
+        } else {
+            for (i in 0..9) {
+                val randomNumber = (Math.random() * DECIMAL_NUMBER_SYSTEM).toInt()
+
+                if (randomNumber == 0) password += i
+            }
+        }
+    }
+
+    println(password)
+
+}
