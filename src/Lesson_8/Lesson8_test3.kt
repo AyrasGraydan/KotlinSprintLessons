0e41723f17513201acc63e2c5849e7d604091476
ayras <AyrasGraydan000@yandex.ru>
2025-09-02 16:57:40 +0300

    KS-8-3-deleted comment

--- src/Lesson_8/Lesson8_test3.kt
+++ src/Lesson_8/Lesson8_test3.kt
@@ -4,11 +4,10 @@ fun main() {
 
     val arrayOfDishes = arrayOf("листья", "гренки", "пармезан", "соус", "мясо", "томаты")
 
-    print("Введите названия блюда: ")
     val userDish = readln()
 
     if (arrayOfDishes.indexOf(userDish) != -1)
         println("Ингредиент \"$userDish\" в рецепте есть")
-    else println("Такого ингредиента в рецепте нет")
+    else println()
 
 }
