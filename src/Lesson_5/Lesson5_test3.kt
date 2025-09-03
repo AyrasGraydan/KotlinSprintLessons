738d24bfb131777f231caa592a3f677c274e9790
ayras <AyrasGraydan000@yandex.ru>
2025-09-03 14:15:25 +0300

    KS-8-5-optimization

--- src/Lesson_5/Lesson5_test3.kt
+++ src/Lesson_5/Lesson5_test3.kt
@@ -1,18 +1,21 @@
 package Lesson_5
 
-import java.io.IO.readln
-
 fun main() {
 
     val numberOne = 28
     val numberTwo = 42
 
-    val possibleNumber1 = readln("Введите возможное число: ").toInt()
-    val possibleNumber2 = readln("Введите возможное число: ").toInt()
+    print("Введите возможное число: ")
+    val possibleNumber1 = readln().toInt()
+
+    print("Введите возможное число: ")
+    val possibleNumber2 = readln().toInt()
 
     when {
-        possibleNumber1 == numberOne && possibleNumber2 == numberTwo -> println("Поздравляем! Вы выиграли главный приз!")
-        possibleNumber1 == numberTwo && possibleNumber2 == numberOne -> println("Поздравляем! Вы выиграли главный приз!")
+        (possibleNumber1 == numberOne && possibleNumber2 == numberTwo) ||
+                (possibleNumber1 == numberTwo && possibleNumber2 == numberOne) ->
+            println("Поздравляем! Вы выиграли главный приз!")
+
         possibleNumber1 == numberOne || possibleNumber2 == numberTwo ||
                 possibleNumber1 == numberTwo || possibleNumber2 == numberOne -> println("Вы выиграли утешительный приз!")
 
