696e623e3713a6c3ebf4f5a8b2c8793b58c14bbe
ayras <AyrasGraydan000@yandex.ru>
2025-09-03 23:32:33 +0300

    KS-8-3-fix naming

--- src/Lesson_8/Lesson8_test3.kt
+++ src/Lesson_8/Lesson8_test3.kt
@@ -2,11 +2,11 @@ package Lesson_8
 
 fun main() {
 
-    val arrayOfDishes = arrayOf("листья", "гренки", "пармезан", "соус", "мясо", "томаты")
+    val arrayOfIngredients = arrayOf("листья", "гренки", "пармезан", "соус", "мясо", "томаты")
 
     val userDish = readln()
 
-    if (arrayOfDishes.indexOf(userDish) != -1)
+    if (arrayOfIngredients.indexOf(userDish) != -1)
         println("Ингредиент \"$userDish\" в рецепте есть")
     else println()
 
