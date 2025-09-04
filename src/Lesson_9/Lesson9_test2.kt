4662746e8ccbd7920ae5cd51bfbc0d72f30b6739
ayras <AyrasGraydan000@yandex.ru>
2025-09-04 22:25:02 +0300

    KS-9-2-list-ingredients

---
+++ src/Lesson_9/Lesson9_test2.kt
@@ -0,0 +1,23 @@
+package Lesson_9
+
+fun main() {
+
+    val listIngredients = mutableListOf("помидоры", "огурцы", "сыр фета")
+
+    println("В рецепте есть базовые ингредиенты: ")
+    listIngredients.forEach { println("№${listIngredients.indexOf(it) + 1} $it") }
+
+    println()
+    print("Желаете добавить еще?: да/skip ")
+    val isAddIngredient = readln().equals("да", true)
+
+    if (isAddIngredient) {
+
+        print("Какой ингредиент вы хотите добавить? ")
+        listIngredients.add(readln())
+
+        println("Теперь в рецепте есть следующие ингредиенты:")
+        listIngredients.forEach { println("№${listIngredients.indexOf(it) + 1} $it") }
+    } else println("Список остался неизменен")
+
+}
