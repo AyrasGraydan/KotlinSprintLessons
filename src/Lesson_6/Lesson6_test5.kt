7e72e0429be67149238d90762d55b1251aee367d
ayras <ayrasgraydan000@yandex.ru>
2025-09-20 15:46:21 +0300

    KS-6-5-optimization

--- src/Lesson_6/Lesson6_test5.kt
+++ src/Lesson_6/Lesson6_test5.kt
@@ -5,31 +5,30 @@ fun main() {
     val minNumber = 1
     val maxNumber = 9
 
-    var number1 = 1
-    var number2 = 1
-    var userNumber = 0
-
     var numberOfAttempts = 3
+    var isResultRight = false
 
     println("Отстои свою честь! Докажи, что ты не бот.")
 
-    while (userNumber != number1 + number2 && numberOfAttempts > 0) {
-
+    do {
         when (numberOfAttempts) {
             2 -> println("Ты справишься, старайся!")
             1 -> println("У тебя еще все шансы, сосредоточься...")
         }
 
-        number1 = (minNumber..maxNumber).random()
-        number2 = (minNumber..maxNumber).random()
+        val number1 = (minNumber..maxNumber).random()
+        val number2 = (minNumber..maxNumber).random()
 
         print("$number1 + $number2 = ")
-        userNumber = readln().toInt()
+        val userNumber = readln().toInt()
 
         numberOfAttempts--
-    }
 
-    if (userNumber == number1 + number2) {
+        if (userNumber == number1 + number2) isResultRight = true
+
+    } while (userNumber != number1 + number2 && numberOfAttempts > 0)
+
+    if (isResultRight) {
         println("Я верил, что ты чист!")
         println("Двери тебе открыты..")
     } else println("Серое и безжизненное существо...")
