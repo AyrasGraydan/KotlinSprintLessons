608b4093a534089796d1c229d28404bff711c576
ayras <ayrasgraydan000@yandex.ru>
2025-09-21 13:02:03 +0300

    KS-6-2-minor corrections

--- src/Lesson_6/Lesson6_test2.kt
+++ src/Lesson_6/Lesson6_test2.kt
@@ -1,17 +1,17 @@
 package Lesson_6
 
-const val MILLISECONDS_PER_MINUTE = 1000
+const val MILLISECONDS_PER_SECOND = 1000
 
 fun main() {
 
     print("Введите количество секунд: ")
-    var pauseTime = readln().toInt()
+    val pauseTime = readln().toLong()
 
-    Thread.sleep(pauseTime.toLong() * MILLISECONDS_PER_MINUTE)
+    Thread.sleep(pauseTime * MILLISECONDS_PER_SECOND)
 
     when {
-        pauseTime % 100 == 1 -> println("Прошла $pauseTime секунда")
+        (pauseTime % 100).toInt() == 1 -> println("Прошла $pauseTime секунда")
         pauseTime % 100 in 2..4 -> println("Прошло $pauseTime секунды")
-        pauseTime % 100 > 4 -> println("Прошло $pauseTime секунд")
+        else -> println("Прошло $pauseTime секунд")
     }
 }
