131a1b436a651bf1e9833834929560511b92675d
ayras <ayrasgraydan000@yandex.ru>
2025-09-22 21:10:07 +0300

    KS-7-1-optimization

--- src/Lesson_7/Lesson7_test1.kt
+++ src/Lesson_7/Lesson7_test1.kt
@@ -5,37 +5,34 @@ const val DECIMAL_NUMBER_SYSTEM = 10
 
 fun main() {
 
+    val passwordLength = 6
     var password = ""
 
     for (i in 1..3) {
 
-        val expectedPasswordLength = password.length + 1
+        while (password.length != passwordLength) {
 
-        while (password.length != expectedPasswordLength) {
+            if (password.length % 2 == 0) {
+                for (i in 'a'..'z') {
+                    val random = (1..LENGTH_ALPHABET).random()
 
-            for (i in 'a'..'z') {
-                val random = (Math.random() * (LENGTH_ALPHABET + 1)).toInt()
+                    if (random == 1) {
+                        password += i
+                        break
+                    }
+                }
+            } else {
+                for (i in 0..9) {
+                    val random = (1..DECIMAL_NUMBER_SYSTEM).random()
 
-                if (random == 1) {
-                    password += i
-                    break
+                    if (random == 1) {
+                        password += i
+                        break
+                    }
                 }
             }
         }
-
-        while (password.length == expectedPasswordLength) {
-
-            for (i in 0..9) {
-                val random = (Math.random() * DECIMAL_NUMBER_SYSTEM + 1).toInt()
-
-                if (random == 1) {
-                    password += i
-                    break
-                }
-            }
-        }
-
     }
-
     println(password)
 }
+
