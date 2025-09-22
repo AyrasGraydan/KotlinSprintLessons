823efadebcba74553a5328ce9691866f5fb17199
ayras <ayrasgraydan000@yandex.ru>
2025-09-22 21:15:36 +0300

    KS-7-3-optimization

--- src/Lesson_7/Lesson7_test3.kt
+++ src/Lesson_7/Lesson7_test3.kt
@@ -5,10 +5,5 @@ fun main() {
     print("Введите конченое число: ")
     val endNumber = readln().toInt()
 
-    for (i in 0..endNumber) {
-        if (i % 2 == 0) {
-            println(i)
-        }
-    }
-
+    for (i in 0..endNumber step 2) println(i)
 }
