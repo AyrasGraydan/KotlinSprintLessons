c856fa03a3c7de9c296782c8f2f2333e9e07e4eb
ayras <ayrasgraydan000@yandex.ru>
2025-09-23 15:52:35 +0300

    KS-7-1-press ctrl + alt + k

--- src/Lesson_7/Lesson7_test1.kt
+++ src/Lesson_7/Lesson7_test1.kt
@@ -8,33 +8,34 @@ fun main() {
     val passwordLength = 6
     var password = ""
 
-    while (password.length != passwordLength){
+    while (password.length != passwordLength) {
 
-            if (password.length % 2 == 0) {
+        if (password.length % 2 == 0) {
 
-                val random = (1..LENGTH_ALPHABET).random()
-                var position = 0
+            val random = (1..LENGTH_ALPHABET).random()
+            var position = 0
 
-                for (i in 'a'..'z') {
-                    position++
-                    if (position == random) {
-                        password += i
-                        break
-                    }
+            for (i in 'a'..'z') {
+                position++
+                if (position == random) {
+                    password += i
+                    break
                 }
-            } else {
-                val random = (1..DECIMAL_NUMBER_SYSTEM).random()
-                var position = 0
+            }
+        } else {
 
-                for (i in 0..9) {
-                    position++
-                    if (position == random) {
-                        password += i
-                        break
-                    }
+            val random = (1..DECIMAL_NUMBER_SYSTEM).random()
+            var position = 0
+
+            for (i in 0..9) {
+                position++
+                if (position == random) {
+                    password += i
+                    break
                 }
             }
         }
+    }
     println(password)
 }
 
