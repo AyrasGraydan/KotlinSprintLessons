1908b37cecbdabaf473d3a8a9aa46ee325f7937f
ayras <ayrasgraydan000@yandex.ru>
2025-09-23 15:51:11 +0300

    KS-7-1-simplifying the code

--- src/Lesson_7/Lesson7_test1.kt
+++ src/Lesson_7/Lesson7_test1.kt
@@ -8,31 +8,33 @@ fun main() {
     val passwordLength = 6
     var password = ""
 
-    for (i in 1..3) {
-
-        while (password.length != passwordLength) {
+    while (password.length != passwordLength){
 
             if (password.length % 2 == 0) {
-                for (i in 'a'..'z') {
-                    val random = (1..LENGTH_ALPHABET).random()
 
-                    if (random == 1) {
+                val random = (1..LENGTH_ALPHABET).random()
+                var position = 0
+
+                for (i in 'a'..'z') {
+                    position++
+                    if (position == random) {
                         password += i
                         break
                     }
                 }
             } else {
-                for (i in 0..9) {
-                    val random = (1..DECIMAL_NUMBER_SYSTEM).random()
+                val random = (1..DECIMAL_NUMBER_SYSTEM).random()
+                var position = 0
 
-                    if (random == 1) {
+                for (i in 0..9) {
+                    position++
+                    if (position == random) {
                         password += i
                         break
                     }
                 }
             }
         }
-    }
     println(password)
 }
 
