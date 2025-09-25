78984fc9b45668a673592038c89d92d44848e1dc
ayras <ayrasgraydan000@yandex.ru>
2025-09-25 21:34:51 +0300

    KS-7-1-fix

--- src/Lesson_7/Lesson7_test1.kt
+++ src/Lesson_7/Lesson7_test1.kt
@@ -1,40 +1,14 @@
 package Lesson_7
 
-const val LENGTH_ALPHABET = 26
-const val DECIMAL_NUMBER_SYSTEM = 10
-
 fun main() {
 
     val passwordLength = 6
     var password = ""
 
-    while (password.length != passwordLength) {
+    for (i in 1..passwordLength) {
 
-        if (password.length % 2 == 0) {
-
-            val random = (1..LENGTH_ALPHABET).random()
-            var position = 0
-
-            for (i in 'a'..'z') {
-                position++
-                if (position == random) {
-                    password += i
-                    break
-                }
-            }
-        } else {
-
-            val random = (1..DECIMAL_NUMBER_SYSTEM).random()
-            var position = 0
-
-            for (i in 0..9) {
-                position++
-                if (position == random) {
-                    password += i
-                    break
-                }
-            }
-        }
+        if (password.length % 2 == 0) password += ('a'..'z').random()
+        else password += (0..9).random()
     }
     println(password)
 }
