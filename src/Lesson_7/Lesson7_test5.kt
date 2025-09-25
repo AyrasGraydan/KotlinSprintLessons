21c77ed60482d5d25eaf19bf2e1884165300194b
ayras <ayrasgraydan000@yandex.ru>
2025-09-25 22:16:30 +0300

    KS-7-5-fix

--- src/Lesson_7/Lesson7_test5.kt
+++ src/Lesson_7/Lesson7_test5.kt
@@ -1,8 +1,5 @@
 package Lesson_7
 
-const val LENGTH_ALPHABET = 26
-const val DECIMAL_NUMBER_SYSTEM = 10
-
 fun main() {
 
     val minLength = 6
@@ -11,41 +8,10 @@ fun main() {
     print("Введите длинну пароля: ")
     val passwordLength = readln().toInt().coerceAtLeast(minLength)
 
-    while (password.length != passwordLength) {
+    for (i in 1..passwordLength) {
 
-        val probability = (1..3).random()
-
-        if (probability != 1) {
-
-            for (i in 'a'..'z') {
-                val randomNumber = (1..LENGTH_ALPHABET).random()
-
-                if (randomNumber == 1) {
-                    val randomNumber = (1..2).random()
-
-                    when (randomNumber) {
-                        1 -> {
-                            password += i.uppercase()
-                            break
-                        }
-
-                        2 -> {
-                            password += i
-                            break
-                        }
-                    }
-                }
-            }
-        } else {
-            for (i in 0..9) {
-                val randomNumber = (1..DECIMAL_NUMBER_SYSTEM).random()
-
-                if (randomNumber == 1) {
-                    password += i
-                    break
-                }
-            }
-        }
+        if ((0..2).random() == 0) password += (0..9).random()
+        else password += (('a'..'z') + ('A'..'Z')).random()
     }
 
     println(password)
