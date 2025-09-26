ba4e21f589802028994ced51a71ef039b846205e
ayras <ayrasgraydan000@yandex.ru>
2025-09-26 22:05:39 +0300

    KS-7-1-optimization

--- src/Lesson_7/Lesson7_test1.kt
+++ src/Lesson_7/Lesson7_test1.kt
@@ -2,13 +2,16 @@ package Lesson_7
 
 fun main() {
 
+    val lowercaseDiapason = 'a'..'z'
+    val numberDiapason = 0..9
+
     val passwordLength = 6
     var password = ""
 
     for (i in 1..passwordLength) {
 
-        if (password.length % 2 == 0) password += ('a'..'z').random()
-        else password += (0..9).random()
+        if (password.length % 2 == 0) password += lowercaseDiapason.random()
+        else password += numberDiapason.random()
     }
     println(password)
 }
