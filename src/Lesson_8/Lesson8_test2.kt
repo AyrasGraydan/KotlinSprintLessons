1926abeffe4ef89272a19f52d8c098aadd0e3622
ayras <ayrasgraydan000@yandex.ru>
2025-09-28 18:56:32 +0300

    KS-8-2-fix

--- src/Lesson_8/Lesson8_test2.kt
+++ src/Lesson_8/Lesson8_test2.kt
@@ -1,19 +1,19 @@
 package Lesson_8
 
+
 fun main() {
 
     val arrayOfIngredients = arrayOf("листья", "гренки", "пармезан", "соус", "мясо", "томаты")
 
     print("Введите названия блюда: ")
-    val userDish = readln()
-    var availabilityOfDish = false
+    val userIngredient = readln().lowercase()
 
-    for (i in arrayOfIngredients) {
-
-        if (userDish == arrayOfIngredients[arrayOfIngredients.indexOf(i)]) availabilityOfDish = !availabilityOfDish
+    arrayOfIngredients.forEach { ingredients ->
+        if (ingredients == userIngredient) {
+            println("Ингредиент \"$userIngredient\" в рецепте есть")
+            return
+        }
     }
 
-    if (availabilityOfDish) println("Ингредиент \"$userDish\" в рецепте есть")
-    else println("Такого ингредиента в рецепте нет")
-
+    println("Такого ингредиента в рецепте нет")
 }
