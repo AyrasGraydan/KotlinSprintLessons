ee65203b2d27a1728320fd6e67f97e8570e8b7ce
ayras <ayrasgraydan000@yandex.ru>
2025-10-01 20:08:46 +0300

    KS-9-5-upgrade

--- src/Lesson_9/Lesson9_test5.kt
+++ src/Lesson_9/Lesson9_test5.kt
@@ -1,14 +1,15 @@
 package Lesson_9
 
 fun main() {
-
     val setIngredients = mutableSetOf<String>()
     val numberOfIngredients = 5
 
     println("Введите $numberOfIngredients ингридиентов: ")
-    for (i in 0 until numberOfIngredients) setIngredients.add(readln())
+    for (i in 1..numberOfIngredients) {
+        print("#$i ")
+        setIngredients.add(readln())
+    }
 
     val printString = setIngredients.sorted().joinToString(", ").replaceFirstChar { it.uppercase() }
     println(printString)
-
 }
