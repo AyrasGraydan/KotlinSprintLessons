e99211aa711456053988f1867d91da35b96913f0
ayras <ayrasgraydan000@yandex.ru>
2025-10-01 19:00:29 +0300

    KS-9-1-remove lines

--- src/Lesson_9/Lesson9_test1.kt
+++ src/Lesson_9/Lesson9_test1.kt
@@ -1,10 +1,8 @@
 package Lesson_9
 
 fun main() {
-
     val listOfIngredients = listOf("листья", "гренки", "пармезан", "соус", "мясо", "томаты")
 
     println("В рецепте есть следующие ингредиенты:")
     listOfIngredients.forEach { println("№${listOfIngredients.indexOf(it) + 1} $it") }
-
 }
