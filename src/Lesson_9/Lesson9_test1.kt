0ebb3ffbbfaa30a6e70e45f961e57b4316c96afc
ayras <ayrasgraydan000@yandex.ru>
2025-10-02 18:42:57 +0300

    KS-9-1-compression

--- src/Lesson_9/Lesson9_test1.kt
+++ src/Lesson_9/Lesson9_test1.kt
@@ -4,7 +4,5 @@ fun main() {
     val ingredients = listOf("листья", "гренки", "пармезан", "соус", "мясо", "томаты")
 
     println("В рецепте есть следующие ингредиенты:")
-    ingredients.forEachIndexed { index, ingredient ->
-        println("#${index + 1} $ingredient")
-    }
+    ingredients.forEachIndexed { index, ingredient -> println("#${index + 1} $ingredient") }
 }
