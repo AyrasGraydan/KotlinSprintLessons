987a7dd70d0ad4dcae43a40a56c3e91279c01bf0
ayras <ayrasgraydan000@yandex.ru>
2025-10-02 18:39:13 +0300

    KS-9-1-optimization

--- src/Lesson_9/Lesson9_test1.kt
+++ src/Lesson_9/Lesson9_test1.kt
@@ -1,8 +1,10 @@
 package Lesson_9
 
 fun main() {
-    val listOfIngredients = listOf("листья", "гренки", "пармезан", "соус", "мясо", "томаты")
+    val ingredients = listOf("листья", "гренки", "пармезан", "соус", "мясо", "томаты")
 
     println("В рецепте есть следующие ингредиенты:")
-    listOfIngredients.forEach { println("№${listOfIngredients.indexOf(it) + 1} $it") }
+    ingredients.forEachIndexed { index, ingredient ->
+        println("#${index + 1} $ingredient")
+    }
 }
