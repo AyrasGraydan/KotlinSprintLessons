7b6661a6a66f4f866c6ed07b10638524256d9e94
ayras <ayrasgraydan000@yandex.ru>
2025-10-11 22:17:05 +0300

    KS-10-5-fix

--- src/Lesson_10/Lesson10_test5.kt
+++ src/Lesson_10/Lesson10_test5.kt
@@ -12,32 +12,27 @@ fun main() {
     val accessToken = authenticateUser(userLogin, userPassword)
     println()
 
-    val cartContents = getCartContents(accessToken)
-    if (cartContents != null) cartContents.forEach { println(it) }
-    else println("неверный логин или пароль")
+    if (accessToken != null) {
+        println("Содержимое корзины: ")
+        getCartContents().forEachIndexed {
+            index, string -> println("${index + 1}. $string") }
+    } else println("неверный логин или пароль:(")
 }
 
 fun generateToken(): String {
-    var token = ""
     val numbers = 0..9
     val chars = 'a'..'z'
     val charsUppercase = 'A'..'Z'
     val fullDiapason = numbers + chars + charsUppercase
 
-    for (i in 1..32) {
-        token += fullDiapason.random()
-    }
+    val token = (1..32).map { fullDiapason.random() }.joinToString("")
     return token
 }
 
 fun authenticateUser(userLogin: String, userPassword: String): String? =
     if (userLogin == LOGIN && userPassword == PASSWORD) generateToken() else null
 
-fun getCartContents(token: String?): List<String>? {
+fun getCartContents(): List<String> {
     val cartContents = listOf("Клавиатура", "Атомный реактор", "Bugatti")
-
-    if (token != null) {
-        return cartContents
-    }
-    return null
+    return cartContents
 }
