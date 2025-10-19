0b191b7807bab04f6a7d8901eb322ccf11a91097
ayras <ayrasgraydan000@yandex.ru>
2025-10-19 20:11:07 +0300

    KS-12-2-fix

--- src/Lesson_12/Lesson12_test2.kt
+++ src/Lesson_12/Lesson12_test2.kt
@@ -1,22 +1,26 @@
 package Lesson_12
 
+
 fun main() {
     val day1 = DayWeather2("01.11.2023", 12, -4, true)
     val day2 = DayWeather2("04.12.2042", 53, -27, false)
+
+    day1.printDayInfo()
+    day2.printDayInfo()
 }
 
 class DayWeather2(
-    day: String,
-    dayTemperature: Int,
-    nightTemperature: Int,
-    isHavePrecipitation: Boolean
+    val day: String,
+    val dayTemperature: Int,
+    val nightTemperature: Int,
+    val isHavePrecipitation: Boolean
 ) {
-    init {
+    fun printDayInfo() {
         println(
-            """
+            """ 
                 День: $day
                     Дневная температура: $dayTemperature
-                    Ночная температура $nightTemperature
+                    Ночная температура $nightTemperature 
                     Наличие осадков $isHavePrecipitation""".trimIndent()
         )
     }
