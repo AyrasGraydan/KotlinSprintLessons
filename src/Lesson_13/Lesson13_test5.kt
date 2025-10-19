e6b8684f9220cfd375b24a07f5f8de799ff12576
ayras <ayrasgraydan000@yandex.ru>
2025-10-19 23:00:19 +0300

    KS-13-5-tel directory

---
+++ src/Lesson_13/Lesson13_test5.kt
@@ -0,0 +1,19 @@
+package Lesson_13
+
+
+fun main() {
+
+    print("Введите номер: ")
+    val telephone = readln().toLongOrNull()
+    val user: User5
+
+    if (telephone != null)
+        user = User5("GGhuman", telephone)
+    else println("Вы обложались и получили NullPointerException")
+}
+
+class User5(
+    val name: String,
+    val telephone: Long,
+    val company: String? = null
+)
