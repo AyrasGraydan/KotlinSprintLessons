6aba74194c04f9ccd9851bbc54b73c1b79757d89
ayras <ayrasgraydan000@yandex.ru>
2025-10-19 22:30:42 +0300

    KS-13-4-tel directory

---
+++ src/Lesson_13/Lesson13_test4.kt
@@ -0,0 +1,49 @@
+package Lesson_13
+
+fun main() {
+
+    val telephoneBook = TelephoneBook(
+        mutableListOf(
+            User4("John", 142141253312L, "YouTube"),
+            User4("Marin", 1L)
+        )
+    )
+
+    telephoneBook.addUser()
+    println()
+    telephoneBook.printInfo()
+}
+
+class User4(
+    val name: String,
+    val telephone: Long?,
+    val company: String? = null
+) {
+    fun printInfo() {
+        println("Имя: $name \nТел: $telephone \nКомпания: ${company ?: "<не указано>"}")
+    }
+}
+
+class TelephoneBook(val telephoneBook: MutableList<User4>) {
+    fun addUser() {
+        print("Введите имя: ")
+        val name = readln()
+        print("Введите номер: ")
+        val telephone = readln().toLongOrNull()
+        print("Введите компанию: ")
+        var company: String? = readln()
+
+        if (company == "") company = null
+
+        if (telephone != null)
+            telephoneBook.add(User4(name, telephone, company))
+        else println("Вы не указали номер телефона, пользователь не был добавлен")
+    }
+
+    fun printInfo() {
+        telephoneBook.forEach {
+            println("Имя: ${it.name} \nТел: ${it.telephone} \nКомпания: ${it.company ?: "<не указано>"}")
+            println()
+        }
+    }
+}
