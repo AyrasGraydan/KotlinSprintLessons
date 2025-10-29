98a42a5fb68e7ce5dbd0e41153b9f1c53cee1a39
ayras <ayrasgraydan000@yandex.ru>
2025-10-29 20:18:02 +0300

    KS-16-1-dice

---
+++ src/Lesson_16/Lesson16_test1.kt
@@ -0,0 +1,15 @@
+package Lesson_16
+
+fun main() {
+    val dice1 = Dice()
+    val dice2 = Dice()
+    dice1.printValue()
+    dice2.printValue()
+}
+
+private class Dice(val value: Int = (1..6).random()) {
+
+    fun printValue() {
+        println("Число кубика: $value")
+    }
+}
