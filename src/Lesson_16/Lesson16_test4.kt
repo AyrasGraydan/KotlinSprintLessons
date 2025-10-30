32a72bbcd8c1f2ccc05ece849dfe3025bc77f38c
ayras <ayrasgraydan000@yandex.ru>
2025-10-30 20:50:08 +0300

    KS-16-4-fix

--- src/Lesson_16/Lesson16_test4.kt
+++ src/Lesson_16/Lesson16_test4.kt
@@ -2,18 +2,19 @@ package Lesson_16
 
 fun main() {
     val order1 = Order(1)
-    println(order1.status)
-    println(order1.sendApplication(order1, "pls change the status of my order"))
-    println(order1.status)
+    println(order1.getStatus())
+    println(order1.sendApplication("pls change the status of my order"))
+    println(order1.getStatus())
 }
 
-class Order(val number: Int, var status: Boolean = false) {
+class Order(private val number: Int, private var status: Boolean = false) {
 
     private fun changeStatus(value: Boolean) = apply { status = value }
+    fun getStatus() = status
 
-    fun sendApplication(numberOfOrder: Order, message: String): String {
+    fun sendApplication(message: String): String {
         if ((1..10).random() > 2) {
-            numberOfOrder.changeStatus(true)
+            this.changeStatus(true)
             return "Ваша заявка принята"
         } else return "Ваша заявка отколонена"
     }
