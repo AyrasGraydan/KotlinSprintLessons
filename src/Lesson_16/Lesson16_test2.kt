c27c2c591021eda82400704b31bba7816b7b42e4
ayras <ayrasgraydan000@yandex.ru>
2025-10-30 20:45:31 +0300

    KS-16-2-fix

--- src/Lesson_16/Lesson16_test2.kt
+++ src/Lesson_16/Lesson16_test2.kt
@@ -1,16 +1,19 @@
 package Lesson_16
 
+import kotlin.math.pow
+
 fun main() {
-    val circle = Circle(3f)
+    val circle = Circle(3.0)
     println("Радиус: ${circle.getRadius()}")
-    println(String.format("Периметр: %.2f", circle.calculateArea()))
+    println(String.format("Площадь: %.2f", circle.calculateArea()))
     println(String.format("Периметр: %.2f", circle.calculatePerimeter()))
 }
 
-private class Circle(private val radius: Float) {
-    private val pi = 3.14
+private const val pi = 3.14
+
+private class Circle(private val radius: Double) {
 
     fun getRadius() = radius
-    fun calculateArea() = radius * radius * pi
+    fun calculateArea() = radius.pow(2.0) * pi
     fun calculatePerimeter() = 2 * radius * pi
 }
