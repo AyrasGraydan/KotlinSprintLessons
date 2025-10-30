2c14bf7c5f6e81ac69f077d7782086dadead52fe
ayras <ayrasgraydan000@yandex.ru>
2025-10-30 20:33:39 +0300

    KS-16-3-fix

--- src/Lesson_16/Lesson16_test3.kt
+++ src/Lesson_16/Lesson16_test3.kt
@@ -3,8 +3,8 @@ package Lesson_16
 fun main() {
     val user = User("qqq", "111")
 
-    printCheckPasswordResult((user.checkPassword("111")))
-    printCheckPasswordResult((user.checkPassword("__xad")))
+    printCheckPasswordResult(user.checkPassword("111"))
+    printCheckPasswordResult(user.checkPassword("__xad"))
 }
 
 class User(val login: String, private val password: String) {
