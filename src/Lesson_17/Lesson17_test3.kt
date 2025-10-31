c7eecfce1802885f6ccd588335ac99793bf085f1
ayras <ayrasgraydan000@yandex.ru>
2025-10-31 23:42:06 +0300

    KS-17-3-directory

---
+++ src/Lesson_17/Lesson17_test3.kt
@@ -0,0 +1,20 @@
+package Lesson_17
+
+fun main() {
+    val directory1 = Directory("user", 32)
+    val directory2 = Directory("user", 32, true)
+
+    println(directory1.name)
+    println(directory2.name)
+}
+
+class Directory(name: String, numberOfFiles: Int, val secretFlag: Boolean = false) {
+
+    val name = Pair(name, numberOfFiles)
+        get() {
+            if (secretFlag){
+                return Pair("скрытая директория", 0)
+            }
+            return field
+        }
+}
