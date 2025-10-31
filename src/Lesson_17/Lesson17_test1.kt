8b4797b8e2e63c64f743ee790d34b07b777bf325
ayras <ayrasgraydan000@yandex.ru>
2025-10-31 23:16:44 +0300

    KS-17-1-quiz

---
+++ src/Lesson_17/Lesson17_test1.kt
@@ -0,0 +1,21 @@
+package Lesson_17
+
+fun main() {
+    val quiz1 = Quiz("Что ты делал?", "Спал")
+    println(quiz1.question)
+    println(quiz1.answer)
+    quiz1.answer = "Не спал"
+    println(quiz1.answer)
+}
+
+class Quiz(question: String?, answer: String?) {
+
+    val question = question
+        get() = field
+
+    var answer = answer
+        get() = field
+        set(value) {
+            field = value
+        }
+}
