8862fdf705088f400e1cfcaadea89b9ef99f4d88
ayras <ayrasgraydan000@yandex.ru>
2025-11-05 21:44:01 +0300

    KS-18-5-fix

--- src/Lesson_18/Lesson18_test5.kt
+++ src/Lesson_18/Lesson18_test5.kt
@@ -2,39 +2,20 @@ package Lesson_18
 
 fun main() {
     val screen1 = Screen()
-    screen1.drawCircle(1, 1)
-    screen1.drawCircle(1.5f, 1.5f)
-    println()
-    screen1.drawSquare(1, 1)
-    screen1.drawSquare(1.5f, 1.5f)
-    println()
-    screen1.drawPoint(1, 1)
-    screen1.drawPoint(1.5f, 1.5f)
+    val figures = listOf(
+        screen1.drawCircle(1, 1), screen1.drawCircle(1.5f, 1.5f),
+        screen1.drawSquare(1, 1), screen1.drawSquare(1.5f, 1.5f),
+        screen1.drawPoint(1, 1), screen1.drawPoint(1.5f, 1.5f)
+    )
+
+    figures.forEach { println(it) }
 }
 
-class Screen() {
-
-    fun drawCircle(x: Int, y: Int) {
-        println("Нарисован круг - $x ; $y")
-    }
-
-    fun drawCircle(x: Float, y: Float) {
-        println("Нарисован круг - $x ; $y")
-    }
-
-    fun drawSquare(x: Int, y: Int) {
-        println("Нарисован квадрат - $x ; $y")
-    }
-
-    fun drawSquare(x: Float, y: Float) {
-        println("Нарисован квадрат - $x ; $y")
-    }
-
-    fun drawPoint(x: Int, y: Int) {
-        println("Нарисована точка - $x ; $y")
-    }
-
-    fun drawPoint(x: Float, y: Float) {
-        println("Нарисована точка - $x ; $y")
-    }
+class Screen {
+    fun drawCircle(x: Int, y: Int) = "Нарисован круг - $x ; $y"
+    fun drawCircle(x: Float, y: Float) = "Нарисован круг - $x ; $y"
+    fun drawSquare(x: Int, y: Int) = "Нарисован квадрат - $x ; $y"
+    fun drawSquare(x: Float, y: Float) = "Нарисован квадрат - $x ; $y"
+    fun drawPoint(x: Int, y: Int) = "Нарисована точка - $x ; $y"
+    fun drawPoint(x: Float, y: Float) = "Нарисована точка - $x ; $y"
 }
