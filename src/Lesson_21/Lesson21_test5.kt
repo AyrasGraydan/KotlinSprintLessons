00d4f0d732f662e779597e9bd0e3ed58365b34e5
AyrasGraydan <145202375+AyrasGraydan@users.noreply.github.com>
2025-11-15 19:22:00 +0300

    KS-21-5-fix

--- src/Lesson_21/Lesson21_test5.kt
+++ src/Lesson_21/Lesson21_test5.kt
@@ -2,11 +2,11 @@ package Lesson_21
 
 fun main() {
     val skills = mapOf("fencing" to 3, "mana reserve" to 5, "luck" to 5)
+    println(mapOf<String, Int>().maxCategory())
     println(skills.maxCategory())
 }
 
 fun Map<String, Int>.maxCategory(): String {
-    val max = this.values.max()
-    this.forEach { if (it.value == max) return it.key }
-    return "error: Couldn't find a skill with maximum experience"
+    val maxExp = values.maxOrNull() ?: return "---map is empty---"
+    return keys.toList()[values.toList().indexOf(maxExp)]
 }
