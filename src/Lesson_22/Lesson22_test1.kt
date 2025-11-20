64f066a08b39f70c737742330f58192876b5fc3f
AyrasGraydan <AyrasGraydan@gmail.com>
2025-11-20 22:26:19 +0300

    KS-22-1-equals

---
+++ src/Lesson_22/Lesson22_test1.kt
@@ -0,0 +1,21 @@
+package Lesson_22
+
+fun main() {
+    val regularBook1 = RegularBook("Хроники Нарнии", "Клайв Стейплз Льюис")
+    val regularBook2 = RegularBook("Хроники Нарнии", "Клайв Стейплз Льюис")
+    val dataBook1 = DataBook("Хроники Нарнии", "Клайв Стейплз Льюис")
+    val dataBook2 = DataBook("Хроники Нарнии", "Клайв Стейплз Льюис")
+    println(regularBook1 == regularBook2)
+    println("${regularBook1.hashCode()} ${regularBook2.hashCode()}")
+
+    println(dataBook1 == dataBook2)
+    println("${dataBook1.hashCode()} ${dataBook2.hashCode()}")
+//    "==" оператор сравнивает хэш код классов
+//    1. тк у обычных классов код генерируется условно рандомно,
+//    то результат false
+//    2. у data классов код генерируется на основе их данных и при одинаковых данных код один,
+//    по этому при сравнении одинаковых data классов результат true
+}
+
+class RegularBook(val bookName: String, val author: String)
+data class DataBook(val bookName: String, val author: String)
